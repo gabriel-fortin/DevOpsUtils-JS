b05d6c26ff2fd83eb8cdc79a66473befe03ec920
Gabriel Fortin <gabriel.14.fortin@gmail.com>
2024-08-21 20:06:47 +0000

    Change the Composable Fetcher to enable extending the key passed to SWR

--- README.md
+++ README.md
@@ -1,36 +1,15 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+A set of little tools to make my life easier with Azure DevOps
 
-## Getting Started
+## `SWR` and fetchers
 
-First, run the development server:
+This project uses `SWR` and builds an infrastructure on top of it to make the 
+different steps of making a request and consuming a response separated into 
+request/response transformers called "middleware". The motivation is that each 
+place in the app may require a different combination of middleware. The notion 
+of middleware is widely understood and this project meets a developer's 
+expectations of what it does.
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
-```
+In addition to middleware, when building a request, it's possible to provide 
+an additional bit to the key provided to SWR. It results on SWR being better 
+informed of when a request's dependency change - when to re-make a request.
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
-
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
-
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
--- src/networking/fetcher.ts
+++ src/networking/fetcher.ts
@@ -1,38 +1,70 @@
-export type FetcherKey = [string]
-export type Fetcher<T> = (key: FetcherKey) => Promise<T>
-type FetcherWithOptions<T> = (key: FetcherKey, options: RequestInit) => Promise<T>
+/** The type of the URL we use with 'fetch'; it is also the first element of 'FetcherKey' */
+export type FetcherUrl = string // we could also include 'URL' and 'Request' in this type
 
-export type Middleware<T1, T2> = (
+/** What we promise ourselves we'll always use as the key (which eventually is passed to SWR) */
+export type FetcherKey = [FetcherUrl, ...any[]]
+
+/** A fetcher accepting request options and returning the specified type */
+export type FetcherWithOptions<TReturn> = (key: FetcherKey, options: RequestInit) => Promise<TReturn>
+
+/** A transformator which can extend or modify options for the request and which can convert the response */
+export type Middleware<TReturn1, TReturn2> = (
     key: FetcherKey,
     options: RequestInit,
-    next: FetcherWithOptions<T1>,
-) => Promise<T2>
+    next: FetcherWithOptions<TReturn1>,
+) => Promise<TReturn2>
 
+/** Allows building a fetcher for SWR in a flexible way by composing middleware */
 interface ComposableFetcherBuilder<TReturn> {
-    with: <TNewReturn> (_: Middleware<TReturn, TNewReturn>) => ComposableFetcherBuilder<TNewReturn>
-    build: () => Fetcher<TReturn>
+    /**
+     * Allows to expand what the bare fetch operations does
+     * and what the key is (to trigger re-fetching when it changes)
+     * @param middleware will enhance the fetching process by adding payload, headers,
+     *  validation, response parsing and much more 
+     * @returns an new instance of the builder which can build an enhanced fetcher
+    */
+    with: <TNewReturn> (_: Middleware<TReturn, TNewReturn>, keyExtension?: any) => ComposableFetcherBuilder<TNewReturn>
+
+    /**
+     * Builds a fetcher which will execute all the composed middleware
+     */
+    build: (url: FetcherUrl | null) => [FetcherKey | null, (key: FetcherKey) => Promise<TReturn>]
 }
 
 class BuilderImpl<TReturn> implements ComposableFetcherBuilder<TReturn> {
-    constructor(innerFetcher: FetcherWithOptions<TReturn>) {
-        this.innerFetcher = innerFetcher
-    }
     innerFetcher: FetcherWithOptions<TReturn>
-    with<TNewReturn>(middleware: Middleware<TReturn, TNewReturn>): ComposableFetcherBuilder<TNewReturn> {
-        return new BuilderImpl((key, opts) => middleware(key, opts, this.innerFetcher))
+    fetcherKeyAdditions: any[]
+
+    constructor(innerFetcher: FetcherWithOptions<TReturn>, fetcherKeyAdditions: any[]) {
+        this.innerFetcher = innerFetcher
+        this.fetcherKeyAdditions = fetcherKeyAdditions
     }
-    build(): Fetcher<TReturn> {
-        return key => this.innerFetcher(key, {} as RequestInit)
+
+    with<TNewReturn>(middleware: Middleware<TReturn, TNewReturn>, keyExtension?: any): ComposableFetcherBuilder<TNewReturn> {
+        return new BuilderImpl(
+            (key, opts) => middleware(key, opts, this.innerFetcher),
+            keyExtension === undefined ? this.fetcherKeyAdditions : [...this.fetcherKeyAdditions, keyExtension],
+        )
+    }
+
+    build(url: FetcherUrl | null): [FetcherKey | null, (_key: FetcherKey) => Promise<TReturn>] {
+        // if 'url' is null, we want SWR to not make a request (because what's the point of making a request with no URL)
+        // SWR will not make a request if the key it receives is null
+        const actualKeyForSwr: FetcherKey | null = (url == null) ? null : [url, this.fetcherKeyAdditions]
+        return [
+            actualKeyForSwr,
+            key => this.innerFetcher(key, {} as RequestInit)
+        ]
     }
 }
 
+// the base of the middleware pipeline
 const coreFetcher: FetcherWithOptions<Response> =
-    (key: FetcherKey, options: RequestInit) => {
-        const [url] = key
+    ([url]: FetcherKey, options: RequestInit) => {
         console.debug(`🚀 ~ coreFetcher ~ URL: ${url} \n options:`, options)
         return fetch(url, options)
     }
 
 export function useBasicComposableFetcher(): ComposableFetcherBuilder<Response> {
-    return new BuilderImpl(coreFetcher)
+    return new BuilderImpl(coreFetcher, [])
 }
