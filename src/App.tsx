90d1264d54dd64e57897fd0e03934bc0451e1615
Gabriel Fortin <gabriel.14.fortin@gmail.com>
2024-10-11 10:32:34 +0000

    Fix the recreated app
    fix configs
    move root code to new root files
    yarn add --dev @types/node
    npm install swr
    npm install @uidotdev/usehooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import reactLogo from "./assets/react.svg"
+import viteLogo from "/vite.svg"
 
-function App() {
-  const [count, setCount] = useState(0)
+import { AddTasks } from "@/addingTasks"
+import { PatAuth } from "@/auth/PatAuth"
+import { PersonalAccessTokenContextProvider } from "@/contexts/PersonalAccessTokenContext"
+import { ProjectUrlContextProvider } from "@/contexts/ProjectUrlContext"
+import { IfWorkItemIdIsSet, WorkItemIdContextProvider } from "@/contexts/WorkItemIdContext"
+import { SelectProjectUrl } from "@/projectUrl/SelectProjectUrl"
+import { SelectWorkItem } from "@/selectWorkItem/SelectWorkItem"
+import { WorkItemAndItsChildren } from "@/showWorkItems/WorkItemAndItsChildren"
 
+import "./App.css"
+
+
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <ProjectUrlContextProvider>
+      <PersonalAccessTokenContextProvider>
+        <WorkItemIdContextProvider>
+          <main className="main">
+            <h1>A tool for chores in DevOps projects</h1>
+            <Card render={<SelectProjectUrl />} />
+            <Card render={<PatAuth />} />
+            <Card render={<SelectWorkItem />} />
+            <IfWorkItemIdIsSet>
+              <Card render={<WorkItemAndItsChildren />} />
+              <Card render={<AddTasks />} />
+            </IfWorkItemIdIsSet>
+          </main>
+          <Logos />
+        </WorkItemIdContextProvider>
+      </PersonalAccessTokenContextProvider>
+    </ProjectUrlContextProvider>
   )
 }
 
-export default App
+const Card: React.FC<{
+  children?: React.ReactNode
+  render?: React.ReactNode
+}> = ({
+  children,
+  render: childrenToRender
+}) => {
+    return (
+      <div className="card">
+        {childrenToRender}
+        {children}
+      </div>
+    )
+  }
+
+const Logos: React.FC = () => {
+  return (
+    <div>
+      <a href="https://vitejs.dev" target="_blank">
+        <img src={viteLogo} className="logo" alt="Vite logo" />
+      </a>
+      <a href="https://react.dev" target="_blank">
+        <img src={reactLogo} className="logo react" alt="React logo" />
+      </a>
+    </div>
+  )
+}
