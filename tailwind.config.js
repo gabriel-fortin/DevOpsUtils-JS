aba989e9369497beee0f117cc4dd7ed4824e5b18
Gabriel Fortin <gabriel.14.fortin@gmail.com>
2024-10-23 19:52:41 +0000

    Add theme to page

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,5 +10,55 @@ export default {
   plugins: [
     require('daisyui'),
   ],
+  daisyui: {
+    themes: [
+      {
+        gabi1: {
+          "primary": "#00c1df",
+          "secondary": "#da68bd",
+          "accent": "#00faff",
+          "neutral": "#3d4451",
+          "base-100": "#ffffff",
+        },
+        gabi2: {
+          "primary": "#0e5378",
+          "secondary": "#60a5fa",
+          "accent": "#f472b6",
+          "neutral": "#3d4451",
+          "base-100": "#103039",
+          "info": "#3b82f6",
+          "success": "#22c55e",
+          "warning": "#f59e0b",
+          "error": "#ef4444",
+        },
+        gabi3: { // yellow accent
+          "neutral": "#3D4351",
+          "base-100": "#323743",
+          "primary": "#293457",
+          "secondary": "#6C93EF",
+          "accent": "#efc86c",
+          "info": "#3b82f6",
+          "success": "#22c55e",
+          "warning": "#f59e0b",
+          "error": "#ef4444",
+        },
+        gabi4: { // pink accent
+          "neutral": "#3D4351",
+          "base-100": "#323743",
+          "primary": "#293457",
+          "secondary": "#6C93EF",
+          "accent": "#F471B5",
+          "info": "#3b82f6",
+          "success": "#22c55e",
+          "warning": "#f59e0b",
+          "error": "#ef4444",
+        },
+      },
+      "light",
+      "dark",
+      "cupcake",
+      "coffee",
+    ],
+  },
 }
 
