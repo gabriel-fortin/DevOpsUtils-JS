4e689ed5c8264a150481bd06bba971878c84a8f5
Gabriel Fortin <gabriel.14.fortin@gmail.com>
2025-05-25 18:29:52 +0000

    Add Task Edit Dialog

--- src/components/addingTasks/ui/TaskItem.tsx
+++ src/components/addingTasks/ui/TaskItem.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { FC } from "react"
+import { FC, useRef } from "react"
 
 import { Task } from "@/dataAccess/addTask"
 
@@ -23,29 +23,75 @@ export const TaskItem: FC<{
       isTaskEditable,
       toggleTaskCheckedState,
     } = useTaskItemLogic(parentWorkItemId, task, events)
+    const dialogRef = useRef<HTMLDialogElement>(null)
 
-    const editTask = () => {
-      alert('TODO: task edit dialog')
+    const showEditTaskDialog = () => {
+      dialogRef.current!.showModal()
     }
 
     return (
-      <label className="block mt-1">
-        {isHttpRequestOngoing
-          ? <span className="loading loading-ring mr-1">.</span>
-          :
-          <input type="checkbox"
-            className="border-2 align-bottom mr-2 checkbox checkbox-sm checkbox-primary checked:checkbox-secondary"
-            disabled={!isTaskToggable}
-            value={task.getTitle()}
-            checked={isTaskChecked}
-            onChange={toggleTaskCheckedState}
-          />
-        }
-        <span onClick={editTask}
-         className={isTaskEditable ? "link link-secondary" : ""}
-        >
+      <>
+        <label className="block mt-1">
+          {isHttpRequestOngoing ? <Loader /> : <Checkbox />}
+          <TaskTitle />
+        </label>
+
+        <TaskEditDialog />
+      </>
+    )
+
+    function Loader() {
+      return (<span className="loading loading-ring mr-1"></span>)
+    }
+
+    function Checkbox() {
+      return (
+        <input type="checkbox"
+          className="border-2 align-bottom mr-2 checkbox checkbox-sm checkbox-primary checked:checkbox-secondary"
+          disabled={!isTaskToggable}
+          value={task.getTitle()}
+          checked={isTaskChecked}
+          onChange={toggleTaskCheckedState}
+        />
+      )
+    }
+
+    function TaskTitle() {
+      const cssClass = isTaskEditable ? "link link-secondary" : ""
+      return (
+        <span onClick={showEditTaskDialog} className={cssClass}>
           {task.getTitle()}
         </span>
-      </label>
-    )
+      )
+    }
+
+    function TaskEditDialog() {
+
+      // TODO: get URL of item in DevOps to make the button navigate to devOps
+      const temporaryStaticUrl = "https://google.com"
+
+      return (
+        <dialog ref={dialogRef} className="modal">
+          <div className="modal-box">
+            <h2 className="text-xl font-bold mb-3">{task.getTitle()}</h2>
+            <p>This task is already added to the current work item</p>
+
+            <div className="modal-action">
+              <form method="dialog">
+                <button className="btn btn-ghost btn-circle absolute right-2 top-2 text-xl">×</button>
+              </form>
+              <a className="btn btn-info btn-outline" target="_blank" href={temporaryStaticUrl}>
+                See in DevOps
+                <span className="text-xl">↗</span>
+              </a>
+            </div>
+          </div>
+
+          <form method="dialog" className="modal-backdrop">
+            <button className="bg-transparent border-0">Cancel</button>
+          </form>
+        </dialog>
+      )
+    }
+
   }
