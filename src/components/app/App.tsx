3f9a278c6cf78a06157092158bd74e01e687b11d
Gabriel Fortin <gabriel.14.fortin@gmail.com>
2025-09-21 21:14:21 +0000

    Perform tailwind upgrade
    yarn global add @tailwindcss/upgrade
    upgrade
    yarn global remove @tailwindcss/upgrade

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -115,7 +115,7 @@ const Card: React.FC<{
   isHighlighted = false,
 }) => {
     return (
-      <div className={`card p-4 m-1 ${isHighlighted ? "border-1 border-accent" : "border-2 border-primary"}`}>
+      <div className={`card p-4 m-1 ${isHighlighted ? "border border-accent" : "border-2 border-primary"}`}>
         {childrenToRender}
         {children}
         {isHighlighted && <span className="absolute left-0.5 top-4 bottom-4 rounded-sm bg-accent w-0.5 animate-pulse"></span>}
