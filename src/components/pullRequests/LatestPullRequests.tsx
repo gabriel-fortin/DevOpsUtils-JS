a98e5f3c03b47e766adb3726b2e10df26cf3ba8f
Gabriel Fortin <gabriel.14.fortin@gmail.com>
2026-03-22 21:40:24 +0000

    Add reviewers' avatars to Latest Pull Requests

--- src/components/pullRequests/LatestPullRequests.tsx
+++ src/components/pullRequests/LatestPullRequests.tsx
@@ -111,32 +111,53 @@ const PrCard: React.FC<{
         pr.pullRequestId)
 
     const requiresAttention = threads && threads.every(not(looksLikeReviewAction))
+    const reviewersAvatars: string[] = unique(
+      threads
+        ?.filter(t => t.properties && t.properties["CodeReviewThreadType"]?.$value === "ReviewersUpdate")
+        .map(t => t.identities[1].imageUrl) || []
+    )
+    const showNewBadge = reviewersAvatars || !isLoading && !error && requiresAttention
 
     return (
-      <div key={pr.pullRequestId}
-          className="mb-2 p-2 border border-base-200 rounded-md
-                    hover:bg-base-200 hover:border-secondary transition-colors duration-200">
-
-        <div className="text-center text-primary-content/30 flex justify-between">
-          <span className="ml-3">#{pr.pullRequestId}</span>
-          {!isLoading && !error && requiresAttention &&
-            <span className="mr-2 badge badge-warning text-black rounded-full">new</span>
-          }
+      <div className="mb-2 p-2 border border-base-200 rounded-md
+                      hover:bg-base-200 hover:border-secondary transition-colors duration-200">
+        {/* HEADER */}
+        <div className="text-primary-content/30
+                        flex flex-wrap justify-end gap-x-1 gap-y-1">
+          {/* PR id */}
+          <span className="ml-3 pr-3 mr-auto justify-self-start">#{pr.pullRequestId}</span>
+          {/* error badge */}
           {(error || !threads) &&
-            <span className="badge-xs badge badge-error rounded-full"></span>
+            <span className="badge badge-error rounded-full animate-pulse tooltip"
+              data-tip={error?.message ?? "Failed to load PR threads"}>
+              err!
+            </span>
           }
+          {/* 'new' badge */}
+          {showNewBadge &&
+            <span className="badge badge-warning text-black rounded-full tooltip"
+              data-tip="new = Has no review comments, might need attention">
+              new
+            </span>
+          }
+          {/* avatars */}
+          {reviewersAvatars.map(imgUrl =>
+            <img key={imgUrl} src={imgUrl} alt="Reviewer Avatar" className="w-6 h-6 rounded-full" />
+          )}
         </div>
 
+        {/* CONTENT */}
         <div className="">
           {pr.title}
         </div>
 
+        {/* FOOTER - creation date */}
         <div className="text-sm text-primary-content/50 flex gap-2 mt-1">
           <span> created </span>
           <span> {new Date(pr.creationDate).toLocaleDateString()} </span>
           <span> {new Date(pr.creationDate).toLocaleTimeString().slice(0, -3)} </span>
         </div>
-
+        {/* FOOTER - author */}
         <div className="text-sm text-primary-content/50 flex gap-2">
           <span> by </span>
           <span> {pr.createdBy.displayName} </span>
@@ -154,7 +175,7 @@ function looksLikeReviewAction(thread: ThreadDto): boolean {
     if (thread.properties["CodeReviewThreadType"].$value === "VoteUpdate") return true
 
     // joining as a reviewer
-    if (thread.properties["CodeReviewThreadType"].$value === "ReviewersUpdate") return true
+    // if (thread.properties["CodeReviewThreadType"].$value === "ReviewersUpdate") return true
   }
 
   return false
@@ -162,4 +183,8 @@ function looksLikeReviewAction(thread: ThreadDto): boolean {
 
 function not<T>(fn: (_x: T) => boolean): (_x: T) => boolean {
   return (x: T) => !fn(x)
+}
+
+function unique<T>(array: T[]): T[] {
+  return Array.from(new Set(array))
 }
